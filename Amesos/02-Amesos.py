a2319c4ef449f5c2ec663cf7ee0a05462bb21b4b
ritukesh <ritukesh@chalmers.se>
2024-05-11 00:31:51 +0200

    updated with constraints handling

--- Amesos/02-Amesos.py
+++ Amesos/02-Amesos.py
@@ -1,7 +1,10 @@
-# 01-AztecOO.py: demonstrates how a direct linear solver 
-# is used. The matrix from 05-EpetraFECrsMatrix.py is used,
-# and the lhs (X) and rhs (B) vectors are defined using the
-# same map. 
+# 02-Amesos.py: demonstrates the use of direct sparse
+# linear solver for the 1D bar problem defined in
+# 05-EpetraFECrsMatrix.py. Additionally, we set dof 0
+# to zero and dof 5 to 1. In this way, the bar is
+# constrained on one end and pulled from the other
+# end. The solution vector should be:
+# [0,1,2] on rank 0, and [3,4,5] on rank 1.
 
 # Import Epetra and Amesos modules from PyTrilinos
 from PyTrilinos import Epetra, Amesos
@@ -40,7 +43,7 @@ EMap = Epetra.Map ( -1, globalElems, 0, comm )
 # non-overlapping Epetra map
 A = Epetra.FECrsMatrix(Epetra.Copy,EMap,1)
 
-# Print empty A
+# Print empty A (for debugging)
 # print(A)
 
 elemMat = Epetra.SerialDenseMatrix(2,2)
@@ -49,9 +52,9 @@ elemMat[0,1] = -1.0
 elemMat[1,0] = -1.0
 elemMat[1,1] =  1.0
 
-# Print the element matrix
-if rank == 0:
-    print('elemMat: {}'.format(elemMat))
+# Print the element matrix (for debugging)
+#if rank == 0:
+#    print('elemMat: {}'.format(elemMat))
 
 # Element partitioning
 if rank == 0:
@@ -74,9 +77,6 @@ for ielem in range(len(elems)):
         A.InsertGlobalValues(dofs[idof], 2,
                              values, indices)
 
-# Modify the matrix to make it non-singular
-A[0,1] = 0.0
-
 # Synchronize
 A.GlobalAssemble()
 A.FillComplete()
@@ -84,35 +84,90 @@ A.FillComplete()
 # Create an Epetra Vector based on the non-overlapping
 # Epetra map
 B = Epetra.Vector(EMap)
-B.Random()
+B.PutScalar(0.0)
 
 # Create an Epetra Vector based on the non-overlapping
 # Epetra map
 X = Epetra.Vector(EMap)
 X.PutScalar(0.0)
 
-print(B)
-print(X)
+# Create the dof constraints
+if rank == 0:
+    cdofs = [0]
+    cvals = [0.0]
+elif rank == 1:
+    cdofs = [5]
+    cvals = [5.0]
 
+# Prepare some Epetra stuff
+inds   = Epetra.IntSerialDenseVector(1)
+vals   = Epetra.SerialDenseVector(1)
+
+# Apply the constraints
+for dof,val in zip(cdofs,cvals):
+
+    # Fill in values
+    vals[0] = val
+
+    # Loop over the rows
+    for irow in range(A.NumGlobalRows()):
+
+        # Check whether this rank holds irow
+        if A.MyGlobalRow(irow):
+
+            # Extract column indices of the irow
+            cols = A.ExtractGlobalRowCopy ( irow )
+
+            # Fill in the column indices
+            inds[0] = irow
+
+            # Update the Epetra Vector B
+            if irow == dof:
+                B.ReplaceGlobalValues(inds,vals)
+            else:
+                # Check if dof column exists in irow
+                if dof in cols[1]:
+                    vals[0]  = -A[irow,dof] * val
+                    B.SumIntoGlobalValues(vals,inds)
+                    A[irow,dof] = 0.0
+
+            # Set all column values in irow to zero
+            cols[0][:] = 0.0
+            A.ReplaceGlobalValues(dof,cols[0],cols[1])
+
+            # Set the diagonal entry to one
+            A[dof,dof] = 1.0
+
+# Finalize matrix A
+A.FillComplete()
+
+# Create a linear problem
 problem = Epetra.LinearProblem( A, X, B )
+
+# Create an Amesos factory
 factory = Amesos.Factory()
 
-# Creates the solver using the Amesos' factory
+# We pick a solver type
 stype = "Amesos_Lapack"
+
+# Check whether the chosen solver is available in factory
 if factory.Query(stype) == False:
     raise NotImplementedError("Selected solver (%s) not supported" % (stype))
+
+# Create the solver for the linear problem
 solver = factory.Create(stype, problem)
 
-# Setting parameters using a Python dictionary. The list of supported
-# parameters can be found on the user's guide.
-amesosList = {"PrintTiming" : True,
-              "PrintStatus" : True
-             }
-solver.SetParameters(amesosList)
+# Set up some solver parameters
+params = {"PrintTiming" : False,
+          "PrintStatus" : False }
+solver.SetParameters(params)
 
-# Note: we don't check here the return parameters for brevity. 
+# Perform the factorizations
 solver.SymbolicFactorization()
 solver.NumericFactorization()
+
+# Solve the problem
 ierr = solver.Solve()
 
-print(X)
+# Print solution (Epetra Vector) X 
+print('X = ',X, 'on rank: ', comm.MyPID())
